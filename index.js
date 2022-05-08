c148786f836a159fd78e178322334637a8d0b0da
Dani <danisuesmith@gmail.com>
2022-05-07 17:35:47 -0700

    commit tree efea635e121d11cf0b223e041eaf5af90b09832b

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(cards, adj) {
+  let messages = [];
+  for (let i = 0; i < cards.length; i++) {
+    messages.push(`Thank you, ${cards[i]}, for the wonderful ${adj} gift!`);
+  }
+  return messages;
+}
+
+function countDown() {
+  let count = 10;
+  while (count >= 0) {
+    console.log(count--);
+  }
+  return countDown;
+}
